45d8e76b20c7602b0efdbe22bebcbe2cda519974
dude <dudelovecamera@gmail.com>
2024-07-11 05:47:27 +0330

    multi user chat room

--- udpclient/Program.cs
+++ udpclient/Program.cs
@@ -14,8 +14,10 @@ class BestUdpClient
 
     private static EndPoint Remote = (EndPoint)sender;
     private static int size = 300;
-
-    private static int AdvSndRcvData(Socket s, byte[] message,
+    
+  
+    
+    private static int SendData(Socket s, byte[] message,
         EndPoint rmtdevice)
     {
         int recv = 0;
@@ -27,6 +29,32 @@ class BestUdpClient
             {
                 s.SendTo(message, message.Length, SocketFlags.None,
                     rmtdevice);
+            }
+            catch (SocketException e)
+            {
+                if (e.ErrorCode == 10054)
+                    recv = 0;
+                else if (e.ErrorCode == 10040)
+                {
+                    Console.WriteLine("Error receiving packet");
+                    size += 100;
+                    recv = 0;
+                }
+            }
+
+            return recv;
+        }
+    }
+
+    private static int RcvData(Socket s, byte[] message,
+        EndPoint rmtdevice)
+    {
+        int recv = 0;
+        int retry = 0;
+        while (true)
+        {
+            try
+            {
                 data = new byte[size];
                 recv = s.ReceiveFrom(data, ref Remote);
             }
@@ -65,51 +93,59 @@ class BestUdpClient
             IPAddress.Parse("127.0.0.1"), 9050);
         Socket server = new Socket(AddressFamily.InterNetwork,
             SocketType.Dgram, ProtocolType.Udp);
-        int sockopt = (int)server.GetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout);
-        Console.WriteLine("Default timeout: {0}", sockopt);
-        server.SetSocketOption(SocketOptionLevel.Socket,
-            SocketOptionName.ReceiveTimeout, 3000);
-        sockopt = (int)server.GetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout);
-        Console.WriteLine("New timeout: {0}", sockopt);
         
-        
-        
-        data = createMessage("welcome to our server");
-        
-        
-        recv = AdvSndRcvData(server, data, ipep);
-        if (recv > 0)
-        {
-            stringData = Encoding.ASCII.GetString(data, 0, recv);
-            Console.WriteLine(stringData);
-        }
-        else
-        {
-            Console.WriteLine("Unable to communicate with remote host");
-            return;
-        }
+        // int sockopt = (int)server.GetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout);
+        // Console.WriteLine("Default timeout: {0}", sockopt);
+        // server.SetSocketOption(SocketOptionLevel.Socket,
+        //     SocketOptionName.ReceiveTimeout, 3000);
+        // sockopt = (int)server.GetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout);
+        // Console.WriteLine("New timeout: {0}", sockopt);
+
+
+        data = createMessage("userJoined");
+        SendData(server, data, ipep);
+
+        ExThread.ipep = ipep;
+        ExThread.server = server;
+        Thread a = new Thread(ExThread.listenThreadMethod);
+        a.Start();
 
         while (true)
         {
             input = Console.ReadLine();
-            if (input == "exit")
-                break;
-            
             data = createMessage(input);
-            recv = AdvSndRcvData(server, data,
-                ipep);
-            if (recv > 0)
-            {
-                stringData = Encoding.ASCII.GetString(data, 0,
-                    recv);
-                Console.WriteLine(stringData);
-            }
-            else
-                Console.WriteLine("Did not receive an answer");
+            SendData(server, data, ipep);
         }
 
         Console.WriteLine("Stopping client");
         server.Close();
+    }
+
+    public class ExThread
+    {
+        public static Socket server { get; set; }
+        public static IPEndPoint ipep { get; set; }
+        public static void listenThreadMethod()
+        {
+            while (true)
+            {
+                int recv;
+                data = new byte[size];
+
+                recv = RcvData(server, data,
+                    ipep);
+                if (recv > 0)
+                {
+                    var stringData = Encoding.ASCII.GetString(data, 0,
+                        recv);
+                    Console.WriteLine((string?)stringData);
+                }
+                else
+                {
+                    Console.WriteLine("Did not receive an answer");
+                }
+            }
+        }
     }
 
     private static byte[] createMessage(string msg)
